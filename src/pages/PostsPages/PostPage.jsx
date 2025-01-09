ed815a03a22dbee48de95732ff92e938c1e99f6d
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-01-09 18:16:44 +0100

    fixed issues

--- src/pages/PostsPages/PostPage.jsx
+++ src/pages/PostsPages/PostPage.jsx
@@ -1,24 +1,8 @@
-import { useEffect } from "react";
-import axios from "axios";
+
 import { useParams } from "react-router-dom";
 
 export const PostPage = () => {
     const params = useParams(); // Recupera i parametri dall'URL
-
-    // Funzione per ottenere i dati dal backend
-    function getPost(id) {
-        axios
-            .get(apiUrl + '/posts/...' + id)
-            .then((res) => setPost(res.data))
-            .catch((error) => console.error(error))
-            .finally(() => console.log("Data fetch completed"));
-    };
-
-    // Eseguiamo la chiamata API al caricamento del componente
-    useEffect(() => {
-        getPost(params.id);
-    }, [params.id]);
-
 
     return (
         <>
