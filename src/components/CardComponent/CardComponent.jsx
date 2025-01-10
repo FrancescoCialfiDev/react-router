4d51d1355fc48abbe5a5c918211b03e3df13974e
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-01-10 16:30:51 +0100

    Added home to sinhle card and buttons for scroll cards

--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -4,20 +4,20 @@ const endPoint = "/posts"
 
 export const CreateCard = ({ data, remove }) => (
     data.map((element) => (
-        element.published && ( // Questa condizione ci permettere di mostrare le card se la proprietà published risulta "true"
-            <div className="col-6 col-md-4 col-lg-3" key={element.id}>
-                <div className="card" id={element.id}>
-                    <img src={element.image} className="card-img-top" alt={element.title} />
-                    <div className="card-body">
-                        <h5 className="card-title">{element.title}</h5>
-                        <p className="card-text m-0">{element.content}</p>
-                        <p className="m-0">{"Available:" + " " + String(element.published)}</p>
-                        <Link to={`${url}${endPoint}/${element.id}`} className="btn btn-primary">Show Details</Link>
-                        <button type="button" className="btn btn-danger mx-1" onClick={() => remove(element.id)}>Delete</button>
-                    </div>
+        //  element.published && ( // Questa condizione ci permettere di mostrare le card se la proprietà published risulta "true"
+        <div className="col-6 col-md-4 col-lg-3" key={element.id}>
+            <div className="card" id={element.id}>
+                <img src={element.image} className="card-img-top" alt={element.title} />
+                <div className="card-body">
+                    <h5 className="card-title">{element.title}</h5>
+                    <p className="card-text m-0">{element.content}</p>
+                    <p className="m-0">{"Available:" + " " + String(element.published)}</p>
+                    <Link to={`${url}${endPoint}/${element.id}`} className="btn btn-primary">Show Details</Link>
+                    <button type="button" className="btn btn-danger mx-1" onClick={() => remove(element.id)}>Delete</button>
                 </div>
             </div>
-        )
+        </div>
+        //  )
     )
     ));
 
--- src/components/CardComponent/SingleCardShow.jsx
+++ src/components/CardComponent/SingleCardShow.jsx
@@ -1,9 +1,12 @@
 // Creiamo il component Card e passiamo data come props e la funzione removeClick
 
+import { Link } from "react-router-dom";
+
 export const SingleCardShow = ({ data }) => (
 
     data.map((element) => (
-        element.published && ( // Questa condizione ci permettere di mostrare le card se la proprietà published risulta "true"
+        // element.published && ( // Questa condizione ci permettere di mostrare le card se la proprietà published risulta "true"
+        <>
             <div className="col-6 col-md-4 col-lg-3" key={element.id}>
                 <div className="card" id={element.id}>
                     <img src={element.image} className="card-img-top" alt={element.title} />
@@ -12,8 +15,15 @@ export const SingleCardShow = ({ data }) => (
                         <p className="card-text m-0">{element.content}</p>
                     </div>
                 </div>
+                <div className="d-flex justify-content-center">
+                    <Link to={"/posts"} className="btn btn-primary mx-2">Torna alla Home</Link>
+                    <Link to={`/posts/${element.id - 1}`} className="btn btn-primary mx-1">&#8678;</Link>
+                    <Link to={`/posts/${element.id + 1}`} className="btn btn-primary mx-1">&#8680;</Link>
+                </div>
             </div>
-        )
+
+        </>
+        // )
     )
     ));
 
